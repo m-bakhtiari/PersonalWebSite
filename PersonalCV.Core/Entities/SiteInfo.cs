87107671768fe886046c944bdc2119c534dd5247
Mohammadreza <bakhtiarimohammadreza2@gmail.com>
2021-08-04 14:54:23 +0430

    Add packages for sql servers

---
+++ PersonalCV.Core/Entities/SiteInfo.cs
@@ -0,0 +1,15 @@
+﻿using PersonalCV.Core.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalCV.Core.Entities
+{
+    public class SiteInfo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public GeneralEnums.GeneralEnum Key { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ PersonalCV.Core/Enums/GeneralEnums.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalCV.Core.Enums
+{
+    public class GeneralEnums
+    {
+        public enum GeneralEnum
+        {
+            AboutUsText = 1,
+            BirthDay = 2,
+            Website = 3,
+            Phone = 4,
+            City = 5,
+            Age = 6,
+            Degree = 7,
+            Email = 8,
+            FreeLance = 9,
+            MyPhoto = 10,
+            CountOfTestProjects = 11,
+            CountOfPublishedProjects = 12,
+            CountOfCustomers = 13,
+            YearsCountOfExperience = 14,
+            FactsText = 15,
+            SkillsText=16,
+        }
+    }
+}
