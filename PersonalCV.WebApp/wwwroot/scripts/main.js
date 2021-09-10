ecfa74f7cceb5885be44c1f135c8e460a263b54a
Mohammadreza <bakhtiarimohammadreza2@gmail.com>
2021-09-10 16:09:19 +0430

    Add scroll to top button in index

--- PersonalCV.WebApp/wwwroot/scripts/main.js
+++ PersonalCV.WebApp/wwwroot/scripts/main.js
@@ -1,43 +1,84 @@
 // Add your javascript here
 // Don't forget to add it into respective layouts where this js file is needed
 
-$(document).ready(function() {
-  AOS.init( {
-    // uncomment below for on-scroll animations to played only once
-    // once: true  
-  }); // initialize animate on scroll library
+$(document).ready(function () {
+    AOS.init({
+        // uncomment below for on-scroll animations to played only once
+        // once: true  
+    }); // initialize animate on scroll library
 });
 
 // Smooth scroll for links with hashes
 $('a.smooth-scroll')
-.click(function(event) {
-  // On-page links
-  if (
-    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-    && 
-    location.hostname == this.hostname
-  ) {
-    // Figure out element to scroll to
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    // Does a scroll target exist?
-    if (target.length) {
-      // Only prevent default if animation is actually gonna happen
-      event.preventDefault();
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000, function() {
-        // Callback after animation
-        // Must change focus!
-        var $target = $(target);
-        $target.focus();
-        if ($target.is(":focus")) { // Checking if the target was focused
-          return false;
-        } else {
-          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-          $target.focus(); // Set focus again
-        };
-      });
+    .click(function (event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            &&
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function () {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
+
+
+//Back to Top Button
+// Set a variable for our button element.
+var scrollToTopButton = document.getElementById('js-top');
+
+// Let's set up a function that shows our scroll-to-top button if we scroll beyond the height of the initial window.
+var scrollFunc = () => {
+    // Get the current scroll value
+    var y = window.scrollY;
+
+    // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
+    if (y > 0) {
+        scrollToTopButton.className = "back-to-top d-flex align-items-center justify-content-center active text-white active";
+    } else {
+        scrollToTopButton.className = "back-to-top d-flex align-items-center justify-content-center active text-white";
     }
-  }
-});
+};
+
+window.addEventListener("scroll", scrollFunc);
+
+var scrollToTop = () => {
+    // Let's set a variable for the number of pixels we are from the top of the document.
+    var c = document.documentElement.scrollTop || document.body.scrollTop;
+
+    // If that number is greater than 0, we'll scroll back to 0, or the top of the document.
+    // We'll also animate that scroll with requestAnimationFrame:
+    // https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+    if (c > 0) {
+        window.requestAnimationFrame(scrollToTop);
+        // ScrollTo takes an x and a y coordinate.
+        // Increase the '10' value to get a smoother/slower scroll!
+        window.scrollTo(0, c - c / 10);
+    }
+};
+
+// When the button is clicked, run our ScrolltoTop function above!
+scrollToTopButton.onclick = function (e) {
+    e.preventDefault();
+    scrollToTop();
+}
